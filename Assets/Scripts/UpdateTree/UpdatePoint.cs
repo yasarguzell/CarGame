cc4542f95ae3c82530b904938bf211e02c2725f4
senacakirli <senacakirli@gmail.com>
2024-07-09 11:24:28 +0300

    car attribute update system is finished

--- Assets/Scripts/UpdateTree/UpdatePoint.cs
+++ Assets/Scripts/UpdateTree/UpdatePoint.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UpdatePoint : MonoBehaviour
 {
-    public void GetOptions()
+    public void GetUpdate(CarAttributes carAttributes)
     {
+        if (GetComponentInChildren<TMP_Text>().text == "Speed") { carAttributes.speedIndex++; }
+        if (GetComponentInChildren<TMP_Text>().text == "HP") { carAttributes.hpIndex++; }
+        if (GetComponentInChildren<TMP_Text>().text == "Defense") { carAttributes.defenseIndex++; }
+        if (GetComponentInChildren<TMP_Text>().text == "GunDamage") { carAttributes.gunDamageIndex++; }
+        if (GetComponentInChildren<TMP_Text>().text == "GunShootingSpeed") { carAttributes.gunShootingSpeedIndex++; }
+        if (GetComponentInChildren<TMP_Text>().text == "ChargerCapacity") { carAttributes.chargerCapacityIndex++; }
 
+        this.gameObject.SetActive(false);
     }
 }

514746028573f83320da0281845333acd7747afe
BerkayUslu <81648931+BerkayUslu@users.noreply.github.com>
2024-07-11 01:37:54 +0300

    Car health and protection after hit implemented

---
+++ Assets/Scripts/Car Scripts/CarController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace CarGame.Car
+{
+    public class CarController : MonoBehaviour
+    {
+        [SerializeField] private int _initialHealth = 3;
+        [SerializeField] private float _protectionTimeInterval = 3;
+        [SerializeField] private bool _isDamagable = true;
+        private int _health;
+
+        private void Awake()
+        {
+            _health = _initialHealth;
+        }
+
+        public void OnCollisionEnter(Collision other)
+        {
+            if (!other.gameObject.CompareTag("Ground") && _isDamagable)
+            {
+                DecreaseHealth();
+                _isDamagable = false;
+                Invoke("EndProtection", _protectionTimeInterval);
+            }
+        }
+
+        private void DecreaseHealth()
+        {
+            _health--;
+        }
+
+        private void EndProtection()
+        {
+            _isDamagable = true;
+        }
+    }
+}

46c4990ad91b71d0e21d48fd1c4b013cf915af27
AlperenEkinci06 <work.alperenekinci@gmail.com>
2024-07-11 14:51:05 +0300

    FlameThrower Added

--- Assets/Scripts/Weapon Scripts/Ballistic.cs
+++ Assets/Scripts/Weapon Scripts/Ballistic.cs
@@ -17,7 +17,10 @@ public class Ballistic : Effect
         EnemyBase eBase;
         if (!other.transform.TryGetComponent<EnemyBase>(out eBase)) { return;}
         eBase.TakeDamage((int)(thisProjectile.damage));
-        Transform particleEffect = GameObject.Instantiate(particleTransform, thisProjectile.targetTransform.position + Vector3.Scale((this.transform.position - thisProjectile.targetTransform.position), this.transform.TransformDirection(new Vector3(0.25f, 1f, -0.1f))), this.transform.rotation);
+        if (particleTransform)
+        {
+            Transform particleEffect = GameObject.Instantiate(particleTransform, thisProjectile.targetTransform.position + Vector3.Scale((this.transform.position - thisProjectile.targetTransform.position), this.transform.TransformDirection(new Vector3(0.25f, 1f, -0.1f))), this.transform.rotation);
+        }
 
         Destroy(this.gameObject);
     }

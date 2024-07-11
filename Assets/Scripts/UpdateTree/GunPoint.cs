aef5a5a4f91a106dc29a0e516fd5f5e0da96938d
senacakirli <senacakirli@gmail.com>
2024-07-12 02:11:51 +0300

    collectible prefabs created

--- Assets/Scripts/UpdateTree/GunPoint.cs
+++ Assets/Scripts/UpdateTree/GunPoint.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,10 @@ using UnityEngine;
 public class GunPoint : MonoBehaviour
 {
     public int weaponType;
+    public float cycleLength = 2f;
+
+    private void Start()
+    {
+        transform.DORotate(new Vector3(0, 360, 0), cycleLength * 0.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+    }
 }

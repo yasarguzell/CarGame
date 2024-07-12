ee47cc0ee37643f0962f698273776027dd92e09d
Yaşar Güzel <guzelyasar07@gmail.com>
2024-07-12 09:37:08 +0300

    Just before commit from the last

--- Assets/Scripts/UpdateTree/GunAttributes.cs
+++ Assets/Scripts/UpdateTree/GunAttributes.cs
@@ -1,3 +1,4 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -6,6 +7,7 @@ using UnityEngine;
 public class GunAttributes : MonoBehaviour
 {
     public string weaponType;
+    public Sprite image;
 
     private int[] gunDamages = new int[] { 1, 2, 3 };
     public int gunDamageIndex = 0;
